abe130a315dff2ff67b1001825fe8e32765a7e20
AKHIL SRIRAM <ariyaramakhil@gmail.com>
2025-04-06 22:14:58 +0530

    Update settings.py

--- emotion_based_music/settings.py
+++ emotion_based_music/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = 'django-insecure-0wxq#)=5((7!+gkt%lxw(_a#2^d4)_z7)4%lobf1z2x5u66#++
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
